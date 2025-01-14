c4516c4c711c0c4196f0e74a1a3fcee5b71ce297
Peehu Mishra <peehumishra1308@gmail.com>
2025-01-14 21:43:53 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/beeuimage/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.beeuimage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 6), 
+    )..repeat(); 
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFD85B),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
+            SizedBox(height: 1,),
+            // Lottie.asset(
+            //   'assets/animations.json', 
+            //   height: 100,
+            //   controller: _controller,
+            //   onLoaded: (composition) {
+            //     _controller.duration = composition.duration; 
+            //   },
+            // ),
+             
+            Image.asset('assets/Untitled design (1).png',height: 500,width: 400,)
+          ],
+        ),
+      ),
+    );
+  }
+}
